df039f76b063d681e427fe1ca3e74b67f6270af6
vagrant <vagrant@u2004dw>
2020-11-11 13:00:16 +0300

    add transition between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,7 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage): 
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
@@ -9,4 +10,4 @@ class MainPage(BasePage):
         
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-        
+ 
